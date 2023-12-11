ac9e04960fe91c929503642e60473e22d2181d1d
harrisazmi <azmi.harris@gmail.com>
2023-12-11 16:02:00 +0800

    prepared express module

--- client/src/Registeration.jsx
+++ client/src/Registeration.jsx
@@ -15,13 +15,13 @@ export default function Registration() {
                 <div className='mt-3'>
                     <label for='Username' className='block text-base mb-2'>Username</label>
                     <input value={username}
-                        onChange={ev => setUsername(ev, target.value)}
+                        onChange={ev => setUsername(ev.target.value)}
                         type="text" id='Username' className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600' placeholder='Enter User Name...'></input>
                 </div>
                 <div className='mt-3'>
                     <label for='Password' className='block text-base mb-2'>Password</label>
                     <input value={password}
-                        onChange={ev => setPassword(ev, target.value)}
+                        onChange={ev => setPassword(ev.target.value)}
                         type="text" id='Password' className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600' placeholder='Enter Password...'></input>
                 </div>
                 <div className='mt-5'>
