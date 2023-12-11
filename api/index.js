411299b6b5ae5ce046930516cf72832354f6ae65
harrisazmi <azmi.harris@gmail.com>
2023-12-11 21:10:49 +0800

    created JWT Token

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userModel = require('./models/User');
+const jwt = require('jsonwebtoken');
 
 //const mongourl = 'mongodb+srv://user1:user1@cluster0.o1lspnq.mongodb.net/?retryWrites=true&w=majority';
 //password inside is not good practice, therefore put in .env
@@ -9,12 +11,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 //console.log(process.env.MONGO_URL); check for process, is it there?
 mongoose.connect(process.env.MONGO_URL);
+jwtSecret = process.env.JWT_SECRET;
 
 
 
 app.get('/test', (req, res) => {
     res.json('test ok')
 });
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const createdUser = await userModel.create({ username, password });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).status(201).json('ok');
+    }) //._id is for mangoose 1st index
+});
+
+
 
 app.listen(3333);
 
