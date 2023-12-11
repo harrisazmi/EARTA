bcea0f3dd08a65a2cbcc485c9db9eb471e265292
harrisazmi <azmi.harris@gmail.com>
2023-12-11 16:46:30 +0800

    finish mongoose connect

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const mongourl = 'mongodb+srv://user1:user1@cluster0.o1lspnq.mongodb.net/?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-
-
+//const mongourl = 'mongodb+srv://user1:user1@cluster0.o1lspnq.mongodb.net/?retryWrites=true&w=majority';
+//password inside is not good practice, therefore put in .env
+// install dotenv
+dotenv.config();
+//console.log(process.env.MONGO_URL); check for process, is it there?
 
 app.get('/test', (req, res) => {
     res.json('test ok')
